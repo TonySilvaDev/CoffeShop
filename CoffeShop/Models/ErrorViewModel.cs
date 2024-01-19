8abf49bc385967d7e57a9988be56131e0db2538e
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-01-19 10:40:45 -0600

    Video 6. Understand Project Structure

--- CoffeShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CoffeShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
