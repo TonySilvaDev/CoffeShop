d89586a3e74e82e373ec6d92ad53b45cf81ec635
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-01-30 17:38:24 -0600

    Video 16. DbContext and DbSet

---
+++ CoffeShop/Data/CoffeShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CoffeShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeShop.Data
+{
+    public class CoffeShopDbContext : DbContext
+    {
+        public CoffeShopDbContext(DbContextOptions<CoffeShopDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
