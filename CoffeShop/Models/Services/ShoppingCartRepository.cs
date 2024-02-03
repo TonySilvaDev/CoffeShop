62285c8df4aa1c5770ebedafebbec1a744ef25a0
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-02-03 17:20:59 -0600

    Video 32. Create Shopping Cart Model Classes

---
+++ CoffeShop/Models/Services/ShoppingCartRepository.cs
@@ -0,0 +1,41 @@
+﻿using CoffeShop.Data;
+using CoffeShop.Models.Interfaces;
+
+namespace CoffeShop.Models.Services
+{
+    public class ShoppingCartRepository : IShoppingCartRepository
+    {
+        private CoffeShopDbContext dbContext;
+        public ShoppingCartRepository(CoffeShopDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public List<ShoppingCartItem>? ShoppingCartItems { get; set; }
+
+        public void AddToCart(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveFromCart(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
