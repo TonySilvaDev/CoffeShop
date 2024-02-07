1d8730875b4062be030f07d444d178d7076f4df2
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-02-06 22:59:44 -0600

    Video 42. Create Order Repository

---
+++ CoffeShop/Models/Services/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using CoffeShop.Data;
+using CoffeShop.Models.Interfaces;
+
+namespace CoffeShop.Models.Services
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private CoffeShopDbContext dbContext;
+        private IShoppingCartRepository shoppingCartRepository;
+        public OrderRepository(CoffeShopDbContext dbContext, IShoppingCartRepository shoppingCartRepository)
+        {
+            this.dbContext = dbContext;
+            this.shoppingCartRepository = shoppingCartRepository;
+        }
+
+        public void PlaceOrder(Order order)
+        {
+            var shoppingCartItems = shoppingCartRepository.GetShoppingCartItems();
+            order.OrderDetails = new List<OrderDetail>();
+            foreach (var item in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    Quantiy = item.Qty,
+                    Product = item.Product,
+                    Price = item.Product.Price
+                };
+                order.OrderDetails.Add(orderDetail);
+            }
+            order.OrderPlaced = DateTime.Now;
+            order.OrderTotal = shoppingCartRepository.GetShoppingCartTotal();
+            
+            dbContext.Orders.Add(order);
+            dbContext.SaveChanges();
+        }
+    }
+}
